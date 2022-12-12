f317e03e4289ae90b9822f74a6507b687c82b069
Matthew McCants <matt@mccants.io>
2022-12-12 07:22:32 -0500

    chore: update lspconfig to use mason

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -4,26 +4,10 @@ if not cmp_status_ok then
 end
 
 require("mason").setup()
-
-mason_lspconfig = require("mason-lspconfig")
-mason_lspconfig.setup({
-  ensure_installed = {
-    "ansiblels",
-    "bashls",
-    "cssls",
-    "dagger",
-    "dockerls",
-    "gopls",
-    "html",
-    "jsonls",
-    "sumneko_lua",
-    "marksman",
-    "pyright",
-    "taplo",
-    "terraformls",
-    "tflint",
-    "tsserver",
-    "yamlls",
-  }
-})
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+  function (server_name)
+    require("lspconfig")[server_name].setup {}
+  end
+}
 
